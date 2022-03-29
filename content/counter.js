18bef9434e119edb57ba42678aab9af68e08ebbc
lebergarrett <lebergarrett@gmail.com>
2022-03-29 10:46:27 -0700

    New API endpoint

--- content/counter.js
+++ content/counter.js
@@ -1,5 +1,5 @@
 async function fetchCount() {
-    let response = await fetch('https://us-central1-lebergarrett-com.cloudfunctions.net/lebergarrett-cloudfunction-9b9aec5');
+    let response = await fetch('https://us-central1-lebergarrett-com.cloudfunctions.net/lebergarrett-cloudfunction-ec9354a');
     let data = await response.text();
     console.log(data);
     document.getElementById("hits").innerHTML = data
