0c011d242c6ad8fa6096f7dd8474c7c66f49cb50
lebergarrett <lebergarrett@gmail.com>
2022-03-31 17:58:09 -0700

    Updated api endpoint

--- content/counter.js
+++ content/counter.js
@@ -1,5 +1,5 @@
 async function fetchCount() {
-    let response = await fetch('https://y3lwtge382.execute-api.us-east-1.amazonaws.com/prod/getcount');
+    let response = await fetch('https://05li2opwre.execute-api.us-east-1.amazonaws.com/prod/getcount');
     let data = await response.text();
     console.log(data);
     document.getElementById("hits").innerHTML = data
